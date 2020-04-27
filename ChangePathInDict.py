b25f4f9e665a8a07349de3fbbce88f4d5811236b
BraunPenguin <mbraun848@gmail.com>
2020-04-27 10:44:47 -0700

    Remove hardcoded basepath, reads from stored path now assuming the folder structure of the project

--- ChangePathInDict.py
+++ ChangePathInDict.py
@@ -4,11 +4,11 @@ import numpy as np  # (pip install numpy)
 from tkinter import Tk, filedialog
 
 
-basePath = "/home/mbraun/NewIS"
+# basePath = "/home/mbraun/NewIS"
 colabBasePath = "/content/drive/My Drive/NewIS"
 root = Tk()
 root.withdraw()
-annotationDictFileName = filedialog.askopenfilename(initialdir=basePath, filetypes=[("Text Dict","*.txt")])
+annotationDictFileName = filedialog.askopenfilename(filetypes=[("Text Dict","*.txt")])
 
 if not annotationDictFileName:
     quit()
@@ -17,9 +17,18 @@ if not annotationDictFileName:
 with open(annotationDictFileName, 'rb') as handle:
    readDictList = pickle.loads(handle.read())
 
+quit()
 for segDict in readDictList:
-    segDict['file_name'] = segDict['file_name'].replace(basePath, colabBasePath)
-    segDict['image_id'] = segDict['image_id'].replace(basePath, colabBasePath)
+    originalNamePath, _ = os.path.split(segDict['file_name'])
+    originalNameParentFolderPath, _ = os.path.split(originalNamePath)
+    originalNameBasePath, _ = os.path.split(originalNameParentFolderPath)
+
+    originalImageIDPath, _ = os.path.split(segDict['image_id'])
+    originalImageIDParentFolderPath, _ = os.path.split(originalImageIDPath)
+    originalImageIDBasePath, _ = os.path.split(originalImageIDParentFolderPath)
+
+    segDict['file_name'] = segDict['file_name'].replace(originalNameBasePath, colabBasePath)
+    segDict['image_id'] = segDict['image_id'].replace(originalImageIDBasePath, colabBasePath)
 
 # newAnnotationDictFileName = 'annotations_dict_bitmask_Train_colab.txt'
 newAnnotationDictFileName = 'annotations_dict_bitmask_Validation_colab.txt'
