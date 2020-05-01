4a8ce51dcf3e3ae4dc88701b628a26d62594b881
BraunPenguin <mbraun848@gmail.com>
2020-05-01 09:04:13 -0700

    Add else for initialdir handling of SetupUI

--- TrainNewData.py
+++ TrainNewData.py
@@ -327,6 +327,8 @@ def setDatasetAndMetadata(baseStr: str, setupoptions: SetupOptions):
 def getFileOrDirList(fileOrFolder: str = 'file', titleStr: str = 'Choose a file', fileTypes: str = None, initialDirOrFile: str = os.getcwd()):
     if os.path.isfile(initialDirOrFile) or os.path.isdir(initialDirOrFile):
         initialDir = os.path.split(initialDirOrFile)[0]
+    else:
+        initialDir = initialDirOrFile
     root = Tk()
     root.withdraw()
     assert fileOrFolder.lower() == 'file' or fileOrFolder.lower() == 'folder', "Only file or folder is an allowed string choice for fileOrFolder"
