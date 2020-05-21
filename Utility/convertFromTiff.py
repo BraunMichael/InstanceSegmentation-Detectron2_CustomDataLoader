c4a6f2eb85fe1ceec18a7c5b22b3c30f754598d5
BraunPenguin <mbraun848@gmail.com>
2020-05-20 17:26:46 -0700

    Update folder name

--- Utility/convertFromTiff.py
+++ Utility/convertFromTiff.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import numpy as np
 import os
-path = '/home/mbraun/NewIS/MaskedImages/2020_02_05_JZL0133_center_007_mask.tif'
+path = '/home/mbraun/InstanceSegmentation-Detectron2/MaskedImages/2020_02_24_MB0234_Andrew_002_cropped_brightened_mask.tif'
 rawImage = Image.open(path)
 npImage = np.array(rawImage) * 255
 npImage = np.array(rawImage)
 
 visImage = Image.fromarray(np.uint8(npImage), mode='L')
-visImage.save("/home/mbraun/NewIS/MaskedImages/2020_02_05_JZL0133_center_007_mask.png", 'PNG')
+visImage.save("/home/mbraun/InstanceSegmentation-Detectron2/MaskedImages/2020_02_24_MB0234_Andrew_002_cropped_brightened_mask.png", 'PNG')
 # os.remove(path)
 print('done')
