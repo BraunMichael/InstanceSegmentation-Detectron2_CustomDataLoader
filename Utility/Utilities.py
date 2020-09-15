e23810fd8d00bfa993089b3adbeb5b04a39dd197
BraunPenguin <mbraun848@gmail.com>
2020-09-15 16:39:40 -0700

    Abstract some functions to Utilities.py

--- Utility/Utilities.py
+++ Utility/Utilities.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 from tkinter import Tk, filedialog
 from torch import load as torchload
 from torch import device as torchdevice
+from shapely.geometry import Polygon
+import numpy as np
 import pickle
 import locale
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
@@ -163,3 +165,66 @@ def checkClassNames(classNamesString, numberClasses):
     else:
         goodColor = (39 / 255, 174 / 255, 96 / 255, 1)
         return True
+
+
+def bboxToPoly(xmin, ymin, xmax, ymax):
+    return [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
+
+
+def getMaskAndBBDicts(outputs):
+    maskDict = {}
+    boundingBoxPolyDict = {}
+    numInstances = len(outputs['instances'])
+    # Loop once to generate dict for checking each instance against all others
+    for (mask, boundingBox, instanceNumber) in zip(outputs['instances'].pred_masks, outputs['instances'].pred_boxes, range(numInstances)):
+        npBoundingBox = np.asarray(boundingBox.cpu())
+        # 0,0 at top left, and box is [left top right bottom] position ie [xmin ymin xmax ymax] (ie XYXY not XYWH)
+        boundingBoxPolyDict[instanceNumber] = Polygon(bboxToPoly(npBoundingBox[0], npBoundingBox[1], npBoundingBox[2], npBoundingBox[3]))
+        maskDict[instanceNumber] = np.asarray(mask.cpu())
+    return maskDict, boundingBoxPolyDict, numInstances
+
+
+def isEdgeInstance(imageRight, imageBottom, instanceBoxCoords, isVerticalSubSection):
+    bottomLeft, bottomRight, topLeft, topRight = instanceBoxCoords
+    instanceBottom = bottomLeft[1]
+    instanceTop = topLeft[1]
+    instanceRight = bottomRight[0]
+    instanceLeft = bottomLeft[0]
+    if isVerticalSubSection:
+        if instanceLeft < 20:
+            # too close to left side
+            return True
+        elif abs(instanceRight - imageRight) < 20:
+            # too close to right side
+            return True
+    else:  # HorizontalSubSection
+        if instanceTop < 20:
+            # too close to top side
+            return True
+        elif abs(instanceBottom - imageBottom) < 20:
+            # too close to bottom side
+            return True
+    return False
+
+
+def getXYFromPolyBox(boundingBoxPoly):
+    topXY = []
+    bottomXY = []
+    boundingBoxXY = boundingBoxPoly.boundary.coords[:-1]
+    boundingBoxXYCentroid = boundingBoxPoly.boundary.centroid.coords[0][1]
+    assert len(boundingBoxXY) == 4, "The polygon used did not have 4 sides"
+    for coords in boundingBoxXY:
+        if coords[1] > boundingBoxXYCentroid:
+            bottomXY.append(coords)
+        else:
+            topXY.append(coords)
+
+    if topXY[0][0] > topXY[1][0]:
+        topXY.reverse()
+    if bottomXY[0][0] > bottomXY[1][0]:
+        bottomXY.reverse()
+    topLeft = topXY[0]
+    topRight = topXY[1]
+    bottomLeft = bottomXY[0]
+    bottomRight = bottomXY[1]
+    return bottomLeft, bottomRight, topLeft, topRight
